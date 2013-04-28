0f22cec0e4bec924a01b527e67204c8d3879a36f
Peicheng Liao <peicheng5@gmail.com>
2013-04-28 22:10:47 +0800

    add partition

--- script/createHiveTableFromDDL.py
+++ script/createHiveTableFromDDL.py
@@ -1,37 +1,37 @@
 #coding=utf-8
 """
-	13.04.26 17:47:03 Peicheng Liao <peicheng5 (a) gmail.com>
-	=========================
-	Goal: given RDB DDL to gen hive table 
-	Usage:
-		python createHiveTableFromDDL.py [ddlfile] [[FIELDDELIMITER]] [[LINEDELIMITER]]
-		ex:
-		python createHiveTableFromDDL.py ../DDL/WEBLOG.ddl
-		python createHiveTableFromDDL.py ../DDL/WEBLOG.ddl '\t'
-		python createHiveTableFromDDL.py ../DDL/WEBLOG.ddl '\t'	'\n'
-	History:
-	
+    13.04.26 17:47:03 Peicheng Liao <peicheng5 (a) gmail.com>
+    =========================
+    Goal: given RDB DDL to gen hive table 
+    Usage:
+        python createHiveTableFromDDL.py [ddlfile] [[FIELDDELIMITER]] [[LINEDELIMITER]]
+        ex:
+        python createHiveTableFromDDL.py ../DDL/WEBLOG.ddl
+        python createHiveTableFromDDL.py ../DDL/WEBLOG.ddl '\t'
+        python createHiveTableFromDDL.py ../DDL/WEBLOG.ddl '\t' '\n'
+    History:
+    
 """
 import sys,os
 if len(sys.argv)<2:
     print '''Goal: given RDB DDL to gen hive table 
 Usage:
-	python createHiveTableFromDDL.py [ddlfile]
-	ex:
-	python createHiveTableFromDDL.py ../DDL/WEBLOG.ddl
-	python createHiveTableFromDDL.py ../DDL/WEBLOG_130426.ddl
+    python createHiveTableFromDDL.py [ddlfile]
+    ex:
+    python createHiveTableFromDDL.py ../DDL/WEBLOG.ddl
+    python createHiveTableFromDDL.py ../DDL/WEBLOG_130426.ddl
 
 '''
     exit()
 """
-	FLAG DEFINE
+    FLAG DEFINE
 """
 #if exec cmd set 1,not set 0 for debug and show cmd
-hiveexecflag=1
-
+hiveexecflag=0
+partitionflag=1
 """
-	DEF
-	====
+    DEF
+    ====
 """
 
 """
@@ -42,11 +42,11 @@ def colorprint(strp):
         print '\033[1;33m'+strp+'\033[0m'
 
 """
-	getDDLlist
-	goal:
-	parse DDLFILE return FIELD NAME LIST
-	input: DDLNAME
-	output:ddlist
+    getDDLlist
+    goal:
+    parse DDLFILE return FIELD NAME LIST
+    input: DDLNAME
+    output:ddlist
 """
 def getDDLlist(DDLNAME):
     NEWDDLFILE=open(DDLNAME)
@@ -63,39 +63,40 @@ def getDDLlist(DDLNAME):
     return NEWDDLLIST
 
 """
-	parse DDL from RDB
+    parse DDL from RDB
 """
 
 #print HIVETABLENAME
 #def createHiveTableCMD(HIVETABLENAME,NEWDDLLIST,FIELDDELIMITER):
 def createHiveTableCMD(*args,**kwargs):
-	'''
-	argslist=['HIVETABLENAME','NEWDDLLIST','FIELDDELIMITER']
-	'''
-	HIVETABLENAME=args[0]
-	NEWDDLLIST=args[1]
-	#print len(args),args
-	CREATECMD="CREATE TABLE IF NOT EXISTS "+HIVETABLENAME+" ("
-	for l in NEWDDLLIST[:-1]:
-		#CM_TX_DT                timestamp          
-		if 'DT' in l:
-			CREATECMD=CREATECMD+'\n'+l+" timestamp,"
-		else:
-			CREATECMD=CREATECMD+'\n'+l+" STRING,"
-	if 'DT' in l:
-		CREATECMD=CREATECMD+"\n"+NEWDDLLIST[-1]+" TIMESTAMP)"
-	else:
-		CREATECMD=CREATECMD+"\n"+NEWDDLLIST[-1]+" STRING)"
-	
-	if len(args)>2:
-		FIELDDELIMITER=args[2]
-		CREATECMD=CREATECMD+"\nROW FORMAT DELIMITED\nFIELDS TERMINATED BY '"+FIELDDELIMITER+"'"
-	if len(args)>3:
-		LINEDELIMITER=args[3]
-		CREATECMD=CREATECMD+"\nLINES TERMINATED BY '"+LINEDELIMITER+"'"
-	return CREATECMD
+    '''
+    argslist=['HIVETABLENAME','NEWDDLLIST','FIELDDELIMITER']
+    '''
+    HIVETABLENAME=args[0]
+    NEWDDLLIST=args[1]
+    #print len(args),args
+    CREATECMD="CREATE TABLE IF NOT EXISTS "+HIVETABLENAME+" ("
+    for l in NEWDDLLIST[:-1]:
+        #CM_TX_DT                timestamp          
+        if 'DT' in l:
+            CREATECMD=CREATECMD+'\n'+l+" timestamp,"
+        else:
+            CREATECMD=CREATECMD+'\n'+l+" STRING,"
+    if 'DT' in l:
+        CREATECMD=CREATECMD+"\n"+NEWDDLLIST[-1]+" TIMESTAMP)"
+    else:
+        CREATECMD=CREATECMD+"\n"+NEWDDLLIST[-1]+" STRING)"
+    if partitionflag == 1 :
+        CREATECMD=CREATECMD+'\nPARTITIONED BY (no int,dt string) '
+    if len(args)>2:
+        FIELDDELIMITER=args[2]
+        CREATECMD=CREATECMD+"\nROW FORMAT DELIMITED\nFIELDS TERMINATED BY '"+FIELDDELIMITER+"'"
+    if len(args)>3:
+        LINEDELIMITER=args[3]
+        CREATECMD=CREATECMD+"\nLINES TERMINATED BY '"+LINEDELIMITER+"'"
+    return CREATECMD
 """
-	execHivecmd
+    execHivecmd
 """
 def execHivecmd(cmd):
         execuser='sudo -u hdfs hive -e "'
@@ -103,27 +104,27 @@ def execHivecmd(cmd):
         print os.popen(cmd).read()
 
 if __name__ == "__main__":
-	print 'lol'
-	colorprint('Test getDDLlist')
-	NEWDDLLIST=getDDLlist(sys.argv[1])
-	print NEWDDLLIST
-	DDLINPUTNAME=sys.argv[1]
-	#WEBLOG_130426
-	#HIVETABLENAME=DDLINPUTNAME.split('/')[-1].split('.')[0]
-	#WEBLOG
-	HIVETABLENAME=DDLINPUTNAME.split('/')[-1].split('_')[0]
-	
-	"""
-	if len(sys.argv)>2:
-		print createHiveTableCMD(HIVETABLENAME,NEWDDLLIST,sys.argv[2])
-	else:
-		print createHiveTableCMD(HIVETABLENAME,NEWDDLLIST)
-	if len(sys.argv)>3:
-		print createHiveTableCMD(HIVETABLENAME,NEWDDLLIST,sys.argv[2],sys.argv[3])
-	"""
-	print createHiveTableCMD(HIVETABLENAME,NEWDDLLIST,*sys.argv[2:])
-	#print createHiveTableCMD(*sys.argv[0:])
-	
-	if hiveexecflag == 1:
-		colorprint('=====execHiveCmd=====')
-		execHivecmd(createHiveTableCMD(HIVETABLENAME,NEWDDLLIST,*sys.argv[2:]))
+    print 'lol'
+    colorprint('Test getDDLlist')
+    NEWDDLLIST=getDDLlist(sys.argv[1])
+    print NEWDDLLIST
+    DDLINPUTNAME=sys.argv[1]
+    #WEBLOG_130426
+    #HIVETABLENAME=DDLINPUTNAME.split('/')[-1].split('.')[0]
+    #WEBLOG
+    HIVETABLENAME=DDLINPUTNAME.split('/')[-1].split('_')[0]
+    
+    """
+    if len(sys.argv)>2:
+        print createHiveTableCMD(HIVETABLENAME,NEWDDLLIST,sys.argv[2])
+    else:
+        print createHiveTableCMD(HIVETABLENAME,NEWDDLLIST)
+    if len(sys.argv)>3:
+        print createHiveTableCMD(HIVETABLENAME,NEWDDLLIST,sys.argv[2],sys.argv[3])
+    """
+    print createHiveTableCMD(HIVETABLENAME,NEWDDLLIST,*sys.argv[2:])
+    #print createHiveTableCMD(*sys.argv[0:])
+    
+    if hiveexecflag == 1:
+        colorprint('=====execHiveCmd=====')
+        execHivecmd(createHiveTableCMD(HIVETABLENAME,NEWDDLLIST,*sys.argv[2:]))
--- script/loadDataFromLocal.py
+++ script/loadDataFromLocal.py
@@ -1,14 +1,17 @@
 #coding=utf-8
 """
-	13.04.26 19:10:15 Peicheng Liao <peicheng5 (a) gmail.com>
-	=========================
-	goal:
-		Load data from local fs to hive table
-	usage:
-		python loadDataFromLocal.py [DATAPATH]
+    13.04.26 19:10:15 Peicheng Liao <peicheng5 (a) gmail.com>
+    =========================
+    goal:
+        Load data from local fs to hive table
+    usage:
+        python loadDataFromLocal.py [DATAPATH]
 
-	hivecmd:
-		 load data local inpath '' into table WEBLOG partition (dt='20130426',logm='1');
+    hivecmd:
+         load data local inpath '' into table WEBLOG partition (dt='20130426',logm='1');
+    history:
+        - 13.04.28 22:09:54 Peicheng Liao : ADD partition
+
 
 """
 import sys,os
@@ -16,36 +19,43 @@ from hivelib import colorprint,getDDLlist,execHivecmd
 if len(sys.argv)<2:
     print '''Goal: given DATEFILE put to hive table
 Usage:
-	python loadDataFromLocal.py [DATFILE]
-	ex:
-	python loadDataFromLocal.py [DATFILE]
+    python loadDataFromLocal.py [DATFILE]
+    ex:
+    python loadDataFromLocal.py [DATFILE]
 '''
     exit()
 #WEBLOG_1_130426.dat
+partitionflag=1
+
 
 #args (DATAPATH,TABLENAME,FILEDDELIMITETER,LINEDELIMITETER)
 def loadDataToHiveTable(*args):
-	print args
-	DATAPATH=args[0]
-	TABLENAME=args[1]
-	loadCMD="LOAD DATA LOCAL INPATH '"+DATAPATH+"'" 
-	loadCMD=loadCMD+' into table '+TABLENAME
-	return loadCMD
+    print args
+    DATAPATH=args[0]
+    TABLENAME=args[1]
+    
+    loadCMD="LOAD DATA LOCAL INPATH '"+DATAPATH+"'" 
+    loadCMD=loadCMD+' into table '+TABLENAME
+    if partitionflag == 1:
+        #../script/WEBLOG_1_130426.dat
+        DT=DATAPATH.split('/')[-1].split('_')[-1].split('.')[0]
+        NO=DATAPATH.split('/')[-1].split('_')[-2]
+        loadCMD=loadCMD+'\nPARTITION (dt="'+DT+'",no='+NO+')'
+    return loadCMD
 
 
 
 
 if __name__ == "__main__":
-
-	execflag=1
-	argvlist=sys.argv[1].split('/')[-1].split('_')
-	print argvlist
-	TABLE_NEME=argvlist[0].split('.')[0]
-	TABLE_DATE=argvlist[2].split('.')[0]
-	print TABLE_NEME,TABLE_DATE
-	print 'lol'
-	print TABLE_NEME,TABLE_DATE
-	print loadDataToHiveTable(sys.argv[1],TABLE_NEME)
-	
-	if execflag == 1:
-		execHivecmd(loadDataToHiveTable(sys.argv[1],TABLE_NEME))
+    execflag=0
+    argvlist=sys.argv[1].split('/')[-1].split('_')
+    print argvlist
+    TABLE_NEME=argvlist[0].split('.')[0]
+    TABLE_DATE=argvlist[2].split('.')[0]
+    print TABLE_NEME,TABLE_DATE
+    print 'lol'
+    print TABLE_NEME,TABLE_DATE
+    print loadDataToHiveTable(sys.argv[1],TABLE_NEME)
+    
+    if execflag == 1:
+        execHivecmd(loadDataToHiveTable(sys.argv[1],TABLE_NEME))
